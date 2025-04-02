e599dd6d8abb30f914a8d38e7556cbdd83697212
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-04-02 09:08:37 +0800

    Update EmalvadoCpmTool.py

--- EmalvadoCpmTool.py
+++ EmalvadoCpmTool.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from elmalvadocarpark import Tresehshs
+from elmalvado import Tresehshs
 
 __CHANNEL_USERNAME__ = "EELMALVADOMAINCHANNEL"
 __GROUP_USERNAME__   = "ElmalvadoMainChat"
